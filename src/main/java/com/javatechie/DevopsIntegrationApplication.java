e73914661d67f262d576ff26c651a61b373de55b
anhpdn2 <anhpdn2@gmail.com>
2023-02-15 16:05:33 +0700

    sss

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "welcome to ducanh";
 	}
 
 	public static void main(String[] args) {
